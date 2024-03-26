5884f8cc8835341731949bc75e3af061869ff9ff
Suyash kanchan <suyashkanchan@gmail.com>
2024-03-26 20:27:56 +0530

    Update 268.cpp

--- 268.cpp
+++ 268.cpp
@@ -1,4 +1,9 @@
-// My Approach Towards the Solution : I used the Bit manipulation to solve the problem. As we know that if we use XOR operator on same numbers that it returns 0, with this I applied the XOR operator on the series of series range [0,n] where n is size of original vector. and then stored the answer and again applied the xor operator on all elements of nums vector. After appiying the XOR operator, like elements will cancel out each other and one odd element will be left, and that is the Answer. 
+// My Approach Towards the Solution :-
+// I used Bit manipulation to solve the problem. As we know that if we use XOR operator on same 
+// numbers, it will return 0, with this I applied the XOR operator on the numbers in series range [0,n]  
+// where n is size of original vector. and then stored the output and again applied the xor operator on  
+// all elements of nums vector. After appiying the XOR operator, like elements will cancel out each 
+// other and one odd element will be left, and that is the Answer. 
 
 class Solution {
 public:
