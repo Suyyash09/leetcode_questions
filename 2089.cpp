aabea837c7a1b4789713313968a58411fb2adf1e
Suyash kanchan <suyashkanchan@gmail.com>
2024-04-10 20:23:28 +0530

    Update 2089.cpp

--- 2089.cpp
+++ 2089.cpp
@@ -1,3 +1,6 @@
+// Approach - First sort the array, then match the target with the elements of array. Whichever elements are equals to target then push its index
+// to the resultant vector.
+
 class Solution
 {
 public:
